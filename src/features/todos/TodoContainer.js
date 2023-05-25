d9229b9aa676006a051f3ab2a67fa417fa653c29
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-05-25 15:19:09 +0300

    read state from slice

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Todo from "./Todo";
+import { useSelector } from "react-redux";
 function TodoContainer() {
-  return <div>TodoContainer</div>;
+
+  const todolist = useSelector((state) => state.todos.entities)
+  
+  return <div>
+    {todolist && todolist.map((todo, index) => {
+     return <Todo todo={todo} key={index} />
+    })}
+  </div>;
 }
 
 export default TodoContainer;
